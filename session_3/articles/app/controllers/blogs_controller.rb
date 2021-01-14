2828bca2b04d5d70a8f359c3c93b30dc52b15b6f
Harshal.S <harshal2015@gmail.com>
2021-01-14 22:33:55 +0530

    Session 3

--- session_3/articles/app/controllers/blogs_controller.rb
+++ session_3/articles/app/controllers/blogs_controller.rb
@@ -14,6 +14,7 @@ class BlogsController < ApplicationController
 
   # GET /blogs/new
   def new
+    @blog = Blog.new
   end
 
   # GET /blogs/1/edit
@@ -23,19 +24,37 @@ class BlogsController < ApplicationController
   # POST /blogs
   # POST /blogs.json
   def create
-
+    @blog = Blog.new(blog_params)
+      if @blog.save
+        format.html { redirect_to @blog, notice: 'Blog #{@blog.title} was successfully created.' }
+        format.json { render :show, status: :created, location: @blog }
+      else
+        format.html { render :new }
+        format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
   end
 
   # PATCH/PUT /blogs/1
   # PATCH/PUT /blogs/1.json
   def update
-
+    if @blog.update(blog_params)
+      flash[:notice] = 'Updated #{@blog.title}' 
+      redirect_to root_path 
+    else
+      flash[:error] = 'Not Updated #{@blog.title}' 
+    end
   end
 
   # DELETE /blogs/1
   # DELETE /blogs/1.json
   def destroy
-   
+    @blog = Blog.find(params[:id])
+    if @blog.delete   
+      flash[:notice] = 'Deleted!'   
+      redirect_to root_path   
+    else   
+      flash[:error] = 'Failed to delete!'   
+    end     
   end
 
   private
@@ -44,4 +63,8 @@ class BlogsController < ApplicationController
       @blog = Blog.find(params[:id])
     end
 
+    # Only allow a list of trusted parameters through.
+    def blog_params
+      params.require(:blog).permit(:title, :article)
+    end
 end
